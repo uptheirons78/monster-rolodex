d619daecef7984d7c217a70434885c9e1ce298be
Mauro Bono <bono.mauro@gmail.com>
2019-07-04 09:53:33 +0200

    Updates

---
+++ precache-manifest.72a5f63a4a24f429085bd118e0ac1606.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9be96fd10097e60569bb1507ac87a6e8",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "2a23b30b448e275cdf17",
+    "url": "/monster-rolodex/static/css/main.ea11875d.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monster-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "2a23b30b448e275cdf17",
+    "url": "/monster-rolodex/static/js/main.af29417b.chunk.js"
+  },
+  {
+    "revision": "344166cfeaf30c2159bd",
+    "url": "/monster-rolodex/static/js/runtime~main.5df944a8.js"
+  }
+]);
